6a2ad037b178642ec4e50ceff9ea90cb4e3ee003
Ammar Yasser Tolba <ammartolba89@192.168.1.6>
2026-05-23 19:22:24 +0300

    update

--- resources/views/components/website/partials/_head.blade.php
+++ resources/views/components/website/partials/_head.blade.php
@@ -24,7 +24,7 @@
 <link rel="stylesheet" href="{{ Path::css('twentytwenty.min.css') }}">
 <link rel="stylesheet" href="{{ Path::css('swiper.min.css') }}">
 <link rel="stylesheet" href="{{ Path::css('main.css') }}">
-<link rel="stylesheet" href="{{ Path::css('all.min.css') }}">
+
 <!-- Toastr -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 <link
